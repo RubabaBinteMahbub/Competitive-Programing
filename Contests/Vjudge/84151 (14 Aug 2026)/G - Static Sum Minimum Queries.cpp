a642d3706d7964116c9f6d16daf0d9d4560564c9
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-08-15 21:42:56 +0600

    Create G - Static Sum Minimum Queries.cpp
    Added a new C++ file for processing static sum minimum queries.

---
+++ Contests/Vjudge/84151 (14 Aug 2026)/G - Static Sum Minimum Queries.cpp
@@ -0,0 +1,8 @@
+/*Given an array of n integers, your task is to process 
+q queries of the form: what is the minimum value in range 
+[a,b]*/
+
+/*Static Range Sum Query → range-এর sum বের করতে হবে।
+Static Range Minimum Query → range-এর minimum বের করতে হবে।*/
+
+/*problem-এর structure একই, কিন্তু concept আলাদা।*/
