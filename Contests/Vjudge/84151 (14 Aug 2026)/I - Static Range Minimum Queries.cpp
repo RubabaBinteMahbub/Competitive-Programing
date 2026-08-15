a5f576fdd5c5fcd18f3f1d03b933dff96c97c2dd
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-08-15 21:30:52 +0600

    Add Static Range Minimum Queries solution

---
+++ Contests/Vjudge/84151 (14 Aug 2026)/I - Static Range Minimum Queries.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#define ll long long
+#define endl '\n'
+
+void solve(){
+ll n, q;
+cin>>n>>q;
+
+vector<ll>prefix(n+1, 0);
+
+for(ll i=1; i<=n; i++){
+   ll x;
+   cin>>x;
+   
+   prefix[i] = prefix[i-1] + x;
+}
+
+while(q--){
+   ll l, r;
+   cin>>l>>r;
+
+   // 1-based
+   cout<<prefix[r] - prefix[l-1]<<endl;
+}
+  
+}
+
+int main(){
+      FAST_IO
+   
+      ll t;
+      t=1;
+      //cin>>t;
+
+      while(t--){
+        solve();
+      }
+
+      return 0;
+}
